9b377f771cac9b9235f8ef3083b205028cd7f7b5
RoggersOgao <roggersog@gmail.com>
2023-09-28 11:06:20 +0300

    final Api

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+
+import styles from "./page.module.scss"
+
+export default function Home(){
+    return(
+        <main className={styles.main}>
+            <h1>Portfolio</h1>
+            <h1>API</h1>
+            <p>all rights reserved</p>
+        </main>
+    )
+}
